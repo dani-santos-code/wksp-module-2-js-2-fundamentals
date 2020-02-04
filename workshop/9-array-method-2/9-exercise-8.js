222514f8c2c7ce10f2e17caf640c344a95ef9955
Dani Santos <santos.adaniele@gmail.com>
2020-02-04 14:45:09 -0500

    array every method

--- workshop/9-array-method-2/9-exercise-8.js
+++ workshop/9-array-method-2/9-exercise-8.js
@@ -7,9 +7,12 @@
 // Returns true if every element of lst is of length at least 5.
 // Otherwise returns false.
 function allLong(lst) {
-    // lst is an array of strings
-
+  // lst is an array of strings
+  return lst.every(el => el.length >= 5);
 }
 // -------------------------------------------------------------------------
-console.log('Q8 - case 1', allLong(['Scott', 'Bob', 'Ric', 'Jim']));
-console.log('Q8 - case 2', allLong(['Cyborg', 'Robin', 'Batman', 'Superman', 'Aquaman', 'Flash']));
+console.log("Q8 - case 1", allLong(["Scott", "Bob", "Ric", "Jim"]));
+console.log(
+  "Q8 - case 2",
+  allLong(["Cyborg", "Robin", "Batman", "Superman", "Aquaman", "Flash"])
+);
