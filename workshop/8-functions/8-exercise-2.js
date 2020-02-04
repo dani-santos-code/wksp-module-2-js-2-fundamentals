141265a56d5b3dd04b15741ce72274bc354d2b8a
Dani Santos <santos.adaniele@gmail.com>
2020-02-04 14:10:54 -0500

    function exercises

--- workshop/8-functions/8-exercise-2.js
+++ workshop/8-functions/8-exercise-2.js
@@ -5,15 +5,16 @@
 // Also, wouldn't it be nice if there were a Math method that could simplify the code....
 // https://www.w3schools.com/jsreF/jsref_obj_math.asp
 
-
-
-
+return Math.max(num1, num2);
 
 // Q2b
-// Without changing the function at all, 
+// Without changing the function at all,
 // can you figure out how we could use it to determine the greater number between 4 integers?
 
-
+function greaterNumber(num1, ...moreNumbers) {
+  return Math.max(num1, ...moreNumbers);
+}
 
 // Q2c
-// Would this work with more integers? 
+// Would this work with more integers?
+// Yes!
