997cb5f708d11b1a934c57940a365678bf494552
Dani Santos <santos.adaniele@gmail.com>
2020-02-04 15:15:20 -0500

    count upperBs

--- workshop/10-anything-goes/10-exercise-1.js
+++ workshop/10-anything-goes/10-exercise-1.js
@@ -4,14 +4,20 @@
 // The first character has position 0, which causes the last one to be found at position string.length - 1.
 // In other words, a two-character string has length 2, and its characters have positions 0 and 1.
 
-
 // Q1
 // Write a function countBs that takes a string as its only argument
 // and returns a number that indicates how many uppercase “B”
 // characters there are in the string.
 
 function countBs(str) {
-
+  const upperB = "B";
+  let counter = 0;
+  str.split("").forEach(letter => {
+    if (letter === upperB) {
+      counter = counter + 1;
+    }
+  });
+  return counter;
 }
 // -------------------------------------------------------------------------
-console.log('Q1: ', countBs('BananaBabyBubbles'));
+console.log("Q1: ", countBs("BananaBabyBubbles"));
